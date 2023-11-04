8cdf3aea5bdb27cb66c383392679b7e297e0e8b6
hj46525 <kuba@hubkiewicz.pl>
2023-11-04 12:49:54 +0100

    update

---
+++ pogodynka/src/Controller/WeatherController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Location;
+use App\Repository\MeasurementRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WeatherController extends AbstractController
+{
+    #[Route('/weather/{country}/{city}', name: 'app_weather')]
+
+    public function city(Location $location, MeasurementRepository $repository): Response
+    {
+        $measurements = $repository->findByLocation($location);
+
+        return $this->render('weather/city.html.twig', [
+            'location' => $location,
+            'measurements' => $measurements,
+        ]);
+    }
+
+}
